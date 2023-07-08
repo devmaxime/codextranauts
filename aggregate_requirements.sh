4ced2517697fa8dfb02639b1638626ff845259eb
front-end-guy-2020 <56974140+0xm3ga@users.noreply.github.com>
2023-07-08 20:14:16 +0100

    updated agg requirements

--- aggregate_requirements.sh
+++ aggregate_requirements.sh
@@ -1,8 +1,23 @@
-# Find all requirements.txt files and concatenate them into one
-echo "" > all_requirements.txt
+# Initialize all_requirements.txt as an empty file
+> all_requirements.txt
 
-for file in $(find . -name requirements.txt)
-do
-    cat $file >> all_requirements.txt
-    echo "" >> all_requirements.txt
-done
+# Find all requirements.txt files
+files=$(find . -name requirements.txt)
+
+# Check if any files were found
+if [ -z "$files" ]; then
+    echo "No requirements.txt files found"
+    exit 1
+else
+    for file in $files; do
+        cat "$file" >> all_requirements.txt
+        # Ensure there is a newline at the end of the file
+        echo "" >> all_requirements.txt
+    done
+fi
+
+# Sort the lines in the file and remove duplicates
+sort all_requirements.txt | uniq > temp_requirements.txt
+
+# Overwrite the original file with the cleaned version and remove the temp file
+mv temp_requirements.txt all_requirements.txt
